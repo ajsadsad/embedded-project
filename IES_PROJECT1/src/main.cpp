fa589da9f5ae6311454b9431cd6bc994232439b1
ajsadsad <z5208382@ad.unsw.edu.au>
2024-04-06 20:01:43 +1100

    Ultrasonic and Debounce working

--- IES_PROJECT1/src/main.cpp
+++ IES_PROJECT1/src/main.cpp
@@ -11,98 +11,104 @@
 #define BUZZER_PWM_OUT PD6
 #define LED_PWM_OUT PD5
 
-#define UST PD2 // Ultrasonic sensor trigger pin
-#define USE PD3 // Ultrasonic sensor echo pin
-#define BTN1 PB5 // Button to turn system on and off
-#define BTN2 PB4 // Button to adjust volume of system
+#define UST PB5 // Ultrasonic sensor trigger pin
+#define USE PB4 // Ultrasonic sensor echo pin
+#define BTN1 PD3 // Button to turn system on and off
+#define BTN2 PD2 // Button to adjust volume of system
 
 int setPrescaler_tc0(char option);
 void set_tc0_mode(char mode);
-void buzzBuzzer(double volume); 
+void buzzBuzzer(double volume);
 void blinkLED(double photores);
 void pwmController(int pulseTime, double buzzer_strength, double led_strength);
+
 void init_adc();
+void init_btns();
+
 uint16_t read_adc();
 double getDistance();
+
+volatile bool btn1_status_old = 1;
+volatile bool btn1_status;
+
+volatile bool btn2_status_old = 1;
+volatile bool btn2_status;
+
+ISR(INT1_vect) {
+  btn1_status = bitRead(PIND, BTN1);
+
+  if(btn1_status != btn1_status_old) {
+    _delay_ms(10);
+    btn1_status = bitRead(PIND, BTN1);
+    if(btn1_status != btn1_status_old) {
+      btn1_status_old = btn1_status;
+      if(btn1_status == 0) {
+        bitInverse(PORTB, PB3);
+        btn1_status_old = 1;
+      }
+    }
+  }
+}
+
+ISR(INT0_vect) {
+  btn2_status = bitRead(PIND, BTN2);
+
+  if(btn2_status != btn2_status_old) {
+    _delay_ms(10);
+    btn2_status = bitRead(PIND, BTN2);
+    if(btn2_status != btn2_status_old) {
+      btn2_status_old = btn2_status;
+      if(btn2_status == 0) {
+        bitInverse(PORTB, PB3);
+        btn2_status_old = 1;
+      }
+    }
+  }
+}
 
 int main()
 {
   init_adc();
-  usart_init(8);
-  
-  bitSet(DDRD, UST);
-  bitClear(DDRD, USE);
-
-  bitClear(DDRB, BTN1);
-  bitSet(PORTB, BTN1);
-  bitClear(DDRB, BTN2);
-  bitSet(PORTB, BTN2);
+  init_btns();
 
   //test LED
   bitSet(DDRB, PB3);
   bitSet(PORTB, PB3);
 
-  int debounceCounter = 0;
-  int secondDebounceCounter = 0;
-  
+  //set sensor
+  bitSet(DDRB, PB5);
+  bitClear(DDRB, PB4);
+
+  sei();
+
+  usart_init(103);
   while(1)
   {
-    int tp = 500; // Pulse time in ms. pt is proportional to distance
+    double distance = getDistance();
+
+    int pt = 2000 * distance; // Pulse time in ms. pt is proportional to distance
     double buzzer_strength = 0.01; // strength is proportional to volume
     double led_strength = (read_adc())/1000.0; // strength is proportional to photoresistor value
-    
-    pwmController(tp, buzzer_strength, led_strength);
-    
-    if(!(bitRead(PINB, BTN1))) {
-      _delay_ms(1);
-      if(!(bitRead(PINB, BTN1))) {
-        debounceCounter++;
-        if(debounceCounter >= 20) {
-          _delay_ms(50);
-          bitInverse(PORTB, PB3);
-          debounceCounter = 0;
-        }
-      } else {
-        debounceCounter = 0;
-      }
-    }
-
-    if(!(bitRead(PINB, BTN2))) {
-      _delay_ms(1);
-      if(!(bitRead(PINB, BTN2))) {
-        secondDebounceCounter++;
-        if(secondDebounceCounter >= 20) {
-          // turn on / off the system.
-          secondDebounceCounter = 0;
-        }
-      } else {
-        secondDebounceCounter = 0;
-      }
-    }
+    pwmController(pt, buzzer_strength, led_strength);
+  }
+}
 
 double getDistance() {
 
   long duration = 0;
-  bitClear(PORTD, PORTD2);
+  bitClear(PORTB, UST);
   _delay_us(5);
-  bitSet(PORTD, PORTD2);
+  bitSet(PORTB, UST);
   _delay_us(10);
-  bitClear(PORTD, PORTD2);
+  bitClear(PORTB, UST);
 
-  while(!(PIND & (1 << PIND3))) {
+  while(!(PINB & (1 << USE))) {
   }
-  while((PIND & (1 << PIND3))) {
+  while((PINB & (1 << USE))) {
     duration = duration + 1;
   }
 
-  double distance = (duration / 2);
-  /*
-    Currently in CMs but maybe we just change it so that it set OCR0A here?
-                  Echo reading
-      OCR0A =  ------------------- x MAX
-                 Echo reading max
-
-  */
+  double distance = ((duration/2) / 29.1) / 400;
   return distance;
 }
 
@@ -116,17 +122,24 @@ double getDistance() {
 void pwmController(int pulseTime, double buzzer_strength, double led_strength) {
   set_tc0_mode(3); // Fast PWM MAX
   setPrescaler_tc0(3); // 64 -> 976.56Hz wave
-  
+
   bitSet(TCCR0A,COM0A1); // Clear OC0A on compare match
   bitSet(TCCR0A,COM0B1); // Clear OC0B on compare match
-                         
+
   // Toggle output ON/OFF for certain pulseTime
   buzzBuzzer(buzzer_strength);
-  _delay_ms(pulseTime);
-
+  int j = 0;
+  while(j < pulseTime) {
+    _delay_ms(1);
+    j++;
+  }
 
   blinkLED(led_strength);
-  _delay_ms(pulseTime);
+  int i = 0;
+  while(i < pulseTime) {
+    _delay_ms(1);
+    i++;
+  }
 }
 
 // Buzzes the Buzzer for a certain pulseTime, at a certain strength determined by volume.
@@ -191,4 +204,25 @@ void set_tc0_mode(char mode)
   /* set WGM0n accordingly */
   TCCR0A |= (0b00000011 & (mode - '0'));
   TCCR0B |= (0b00001000 & ((mode - '0') << 1));
+}
+
+void init_btns() {
+  bitSet(DDRD, UST);
+  bitClear(DDRD, USE);
+
+  bitClear(DDRD, BTN1);
+  bitSet(PORTD, BTN1);
+
+  bitClear(DDRD, BTN2);
+  bitSet(PORTD, BTN2);
+
+  //interrupt for INT1
+  bitSet(SREG, SREG_I);
+  bitSet(EICRA, ISC11);
+  bitClear(EICRA, ISC10);
+  bitSet(EIMSK, INT1);
+  //Interrupt for INT0
+  bitSet(EICRA, ISC01);
+  bitClear(EICRA, ISC00);
+  bitSet(EIMSK, INT0);
 }
