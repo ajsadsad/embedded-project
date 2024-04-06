6826e2ddad667cd5bea2c05adf27a389b502d0a8
Vanilla Viking <ashwinr2k2@gmail.com>
2024-04-06 12:20:51 +1100

    wipfeat: alternating led and buzzer

--- IES_PROJECT1/src/main.cpp
+++ IES_PROJECT1/src/main.cpp
@@ -7,38 +7,58 @@
 #include "bit.h"
 
 #define MAX 255
-#define PWMOUT PD6
+#define BUZZER_PWM_OUT PD6
+#define LED_PWM_OUT PD5
 
 int setPrescaler_tc0(char option);
 void set_tc0_mode(char mode);
-void buzzBuzzer(int pulseTime, double volume); 
+void buzzBuzzer(double volume); 
+void blinkLED(double photores);
+void pwmController(int pulseTime, double buzzer_strength, double led_strength);
 
 int main()
 {
   while(1)
   {
     int tp = 500; // Pulse time in ms. pt is proportional to distance
-    double strength = 0.01; // strength is proportional to volume
-    buzzBuzzer(tp,strength);
+    double buzzer_strength = 1; // strength is proportional to volume
+    double led_strength = 1; // strength is proportional to photoresistor value
+    
+    pwmController(tp, buzzer_strength, led_strength);
   }
 }
 
-// Buzzes the Buzzer for a certain pulseTime, at a certain strength determined by volume.
-void buzzBuzzer(int pulseTime, double volume)
-{
+/*
+ * 
+ * */
+void pwmController(int pulseTime, double buzzer_strength, double led_strength) {
   set_tc0_mode(3); // Fast PWM MAX
+  setPrescaler_tc0(3); // 64 -> 976.56Hz wave
+  
+  // Toggle output ON/OFF for certain pulseTime
+  buzzBuzzer(1.0);
+  _delay_ms(pulseTime);
+
+
+  blinkLED(1.0);
+  _delay_ms(pulseTime);
+}
+
+// Buzzes the Buzzer for a certain pulseTime, at a certain strength determined by volume.
+void buzzBuzzer(double volume)
+{
+  bitClear(DDRD,LED_PWM_OUT);
+  bitSet(DDRD,BUZZER_PWM_OUT);
   bitSet(TCCR0A,COM0A1); // Clear OC0A on compare match
   OCR0A = volume*(MAX/2); // Set OCR0A accordingly to volume(duty cycle)
-  setPrescaler_tc0(3); // 64 -> 976.56Hz wave
+}
 
-  // Toggle output ON/OFF for certain pulseTime
-  bitSet(DDRD,PWMOUT);
-  _delay_ms(pulseTime);
-  bitClear(DDRD,PWMOUT);
-  _delay_ms(pulseTime);
-
-  set_tc0_mode(0);
-  setPrescaler_tc0(0);
+void blinkLED(double photores) {
+  bitClear(DDRD,BUZZER_PWM_OUT);
+  bitSet(DDRD,LED_PWM_OUT);
+  bitSet(TCCR0A,COM0B1); // Clear OC0B on compare match
+  bitClear(TCCR0A,COM0B0); // Clear OC0B on compare match
+  OCR0A = photores*(MAX/2); // Set OCR0A accordingly to volume(duty cycle)
 }
 
 int setPrescaler_tc0(char option)
